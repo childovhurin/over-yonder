602db0921c1260de1bd760c299ddf2f89e0be55d
aaivey22 <angelawings22@gmail.com>
2020-03-22 16:53:55 -0400

    merging with master

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', function() {
-    M.AutoInit();
-    var options = {
-      fullWidth: true,
-      indicators: true
-    };
-    var elems = document.querySelector('.carousel.no-autoinit');
-    console.log(elems)
-    var instances = M.Carousel.init(elems, options);
-  })
+$(document).ready(function(){
+    $('.carousel').carousel({fullWidth:true}).css("height", $(window).height());
+    // for next slide
+    $('.next').click(function(){
+        $('.carousel').carousel('next');
+    });
+  // for prev slide
+  $('.prev').click(function(){
+        $('.carousel').carousel('prev');
+    });
+});
 var zipCodeURL = "https://www.zipcodeapi.com/rest/8y6B27OD8VmkCAQ2CbRKlIpO71WQAZxLihMB3eCJHlr0uhrQp3FkVoDtilpAwlus/info.json/23221/degrees" 
-=======
 var queryURL = "https://www.hikingproject.com/data/get-trails?lat=37.5407&lon=-77.4360&maxDistance=10&key=200166394-792a17647727b298af1948b543b6c58c"
-var zipCodeURL = "https://www.zipcodeapi.com/rest/8y6B27OD8VmkCAQ2CbRKlIpO71WQAZxLihMB3eCJHlr0uhrQp3FkVoDtilpAwlus/info.json/23221/degrees"
->>>>>>> 89a326385dfc473b3aefa2ebf747acaf71ef9cc8
 
 
 
@@ -29,13 +26,23 @@ function getPosition(position) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        $('#textarea1').text(JSON.stringify(response.trails[0].name));
-        $('#textarea1').text(JSON.stringify(response.trails[0].summary));
-        $('#textarea1').text(JSON.stringify(response.trails[0].difficulty));
-        $('#textarea1').text(JSON.stringify(response.trails[0].imgSqSmall));
-        $('#textarea1').text(JSON.stringify(response.trails[0].length));
-        $('#textarea1').text(JSON.stringify(response.trails[0].ascent));
-        $('#textarea1').text(JSON.stringify(response.trails[0].descent));
+
+        for (var i = 0; i < 6; i++);
+        
+        var name = response.trails[i].name;
+        var trails = response.trails[i].summary;
+        var difficulty = response.trails[i].difficulty;
+        var imgSqSmall = response.trails[i].imgSqSmall;
+        var length = response.trails[i].length;
+        var ascent = response.trails[i].ascent;
+        var descent = response.trails[i].descent;
+
+        var card = $("<div>");
+        card.empty();
+        var hikingInfo = $(`${hikeImg}
+        ${hikeName}`)
+        card.append(hikingInfo);
+    }
 
 
 
